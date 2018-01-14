c76b5266268cada1d35005dcdc3e55a63d650143
Carl E McGruder <cem@cem.us>
2018-01-14 21:54:25 +0000

    2.2 Initial Thor Options and Arguments
    We'll run the following:
    
    ```
    $ ruby app.rb list_recipes
    ```
    
    We'll see all the recipes that we've entered in the array with
    output layout in the do block loop.
    
    -------------------
    Recipe: Ratatouille
    It takes: 60 min to cook
    The ingredients are: potatoes, carrots, onions, zucchini, tomatoes
    --------------------
    Recipe: Mac & Cheese                                                                                   .
    It takes: 20 min to cook
    The ingredients are: macarroni, cheese, onions, mustard, milk                                           --------------------
    Recipe: Caesar Salad
    It takes: 10 min to cook                                                                                The ingredients are: chicken, lettuce, croutons, eggs
    
    This means that the code is working with the keyword is nil
    
    Now if we run:
    
    ```
    $ ruby app.rb list_recipes oui
    ```
    
    -------------------
    Recipe: Ratatouille
    It takes: 60 min to cook
    The ingredients are: potatoes, carrots, onions, zucchini, tomatoes
    
    As we can see, we only the title that contains 'oui'

--- app.rb
+++ app.rb
@@ -3,7 +3,46 @@ require "thor"
 class App < Thor
  desc "hello WORD", "Print 'Hello, WORD' to the screen."
  def hello word
-	puts "Hello #{word}"
+  puts "Hello #{word}"
+ end
+
+ desc "list_recipes [KEYWORD][OPTIONS]", "List all recipes. If the keyword is given, it filters the list based off it"
+ def list_recipes keyword=nil
+        recipes = [
+	 { 
+            title: "Ratatouille",
+            cooking_time: "60 min",
+            ingredients: %w{potatoes carrots onions zucchini tomatoes}
+          }, 
+	    {
+            title: "Mac & Cheese",
+            cooking_time: "20 min",
+            ingredients: %w{macarroni cheese onions mustard milk}
+          }, 
+	    {
+            title: "Caesar Salad",
+            cooking_time: "10 min",
+            ingredients: %w{chicken lettuce croutons eggs}
+          }
+	]
+
+	
+        recipes_to_be_listed = if keyword.nil? then recipes
+                               else recipes.select {|recipe| recipe[:title].downcase.include? keyword.downcase}
+
+			       end
+
+
+	recipes_to_be_listed.each do |recipe|
+		puts "--------------------"
+		puts "Recipe: #{recipe[:title]}"
+		puts "It takes: #{recipe[:cooking_time]} to cook"
+		puts "The ingredients are: #{recipe[:ingredients].join(", ")}"
+		puts ""
+	end
+
+			         
  end
 end
+
 App.start ARGV
