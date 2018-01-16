12ba2b358ced0c3fe2b6119288b890bd313e07ba
BuffaloBorn <carl.mcgruder@gmail.com>
2018-01-16 00:05:29 +0000

    2.3 - Refactor list method - Thor Subcommands
    After moving the list_recipes method/command to Recipes class.
    We've can test the code and by issuing the following:
    
    ```
    $ ruby app.rb recipes add
    ```
    
    No value provided for required options '--title', '--cooking-time',
    '--description'
    
    ```
    $ ruby app.rb recipes
    ```
    Commands:
      app.rb recipes add --title --cooking-time --description --cooking-time=COOKING_TIME --description=DESCRIPTION --title=TITLE
      app.rb recipes help [COMMAND]
      app.rb recipes list [KEYWORD][OPTIONS]
    
    The code is working as before but there is dups in the add method

--- app.rb
+++ app.rb
@@ -36,13 +36,57 @@ class Recipes < Thor
    RECIPES << recipe
 
 
-  RECIPES.each do |recipe|
-    puts recipe[:title]
-  end
+   RECIPES.each do |recipe|
+     puts recipe[:title]
+   end
+ end
+
+ 
+ desc "list [KEYWORD][OPTIONS]", "List all recipes. If the keyword is given, it filters the list based off it"
+ option :format
+ option :show_time, type: :boolean, default:true #--show-time --no-show-time
+ def list keyword=nil
+    recipes = RECIPES
+    recipes_to_be_listed = if keyword.nil? then recipes
+                            else recipes.select {|recipe| recipe[:title].downcase.include? keyword.downcase}
+			     end
+
+    recipes_to_be_listed.each do |recipe|
+      if options[:format].nil?
+	  print_default recipe	
+      else options[:format] == "oneline"
+	  print_oneline recipe
+       end
+    end   
+
  end
 
 
+
+ private 
+
+  def print_default recipe
+	puts "--------------------"
+	puts "Recipe: #{recipe[:title]}"
+	puts "It takes: #{recipe[:cooking_time]} to cook"
+	puts "The ingredients are: #{recipe[:ingredients].join(", ")}"
+        puts ""
+  end
+
+  def print_oneline recipe                    
+      if options[:show_time]
+        time = "(#{recipe[:cooking_time]})"
+      else
+        time = ""
+      end
+      
+      puts %Q{#{recipe[:title]} #{time}}
+  end
 end
+
+
+
+
 
 class App < Thor
  desc "hello WORD", "Print 'Hello, WORD' to the screen."
@@ -52,50 +96,6 @@ class App < Thor
 
  desc "recipes", "Manage recipes"
  subcommand "recipes", Recipes  # app.rb recipes list
-				# app.rb recipes add 
-				
-
- desc "list_recipes [KEYWORD][OPTIONS]", "List all recipes. If the keyword is given, it filters the list based off it"
- option :format
- option :show_time, type: :boolean, default:true #--show-time --no-show-time
- def list_recipes keyword=nil
-	
-      recipes = RECIPES
-      recipes_to_be_listed = if keyword.nil? then recipes
-                             else recipes.select {|recipe| recipe[:title].downcase.include? keyword.downcase}
-
-			       end
-
-
-	recipes_to_be_listed.each do |recipe|
-	 if options[:format].nil?
-	  print_default recipe	
-	 else options[:format] == "oneline"
-	  print_oneline recipe
-	 end
-	end
-
-			         
- end
-
- private 
-
-  def print_default recipe
-	puts "--------------------"
-	puts "Recipe: #{recipe[:title]}"
-	puts "It takes: #{recipe[:cooking_time]} to cook"
-	puts "The ingredients are: #{recipe[:ingredients].join(", ")}"
-	puts ""
-  end
-
-  def print_oneline recipe
-     if options[:show_time]
-        time = "(#{recipe[:cooking_time]})"
-     else
-	time =""
-     end
-	puts %Q{#{recipe[:title]} #{time}}
-  end
 
 end
 
